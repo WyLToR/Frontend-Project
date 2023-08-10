6057bace43e8a85d4a45ea1d6e02e2e611306e8c
WyLToR <bekesi.patrik@gmail.com>
2023-08-10 11:12:35 +0200

    bootstrap add

--- src/components/SortBox.js
+++ src/components/SortBox.js
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { CurrentPageContext } from "../contexts/CurrentPageContext";
 import { useSearchParams } from "react-router-dom";
+import { Form } from 'react-bootstrap'
 import "./SortBox.css"
 
 
@@ -19,14 +20,20 @@ export default function SortBox({ sortState, setSortState }) {
 
     return (
         <div className="sort-container">
-            <select onChange={handleSortInput} name="sort" value={sortState?.sort} >
-                <option value="name">Név szerint</option>
-                <option value="price">Ár</option>
-            </select>
-            <select onChange={handleSortInput} name="order" value={sortState?.order} >
-                <option value="asc">Növekvő</option>
-                <option value="desc">Csökkenő</option>
-            </select>
+            <Form>
+                <Form.Group>
+                    <Form.Control as="select" onChange={handleSortInput} name="sort" value={sortState?.sort}>
+                        <option value="name">Név szerint</option>
+                        <option value="price">Ár</option>
+                    </Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Control as="select" onChange={handleSortInput} name="order" value={sortState?.order}>
+                        <option value="asc">Növekvő</option>
+                        <option value="desc">Csökkenő</option>
+                    </Form.Control>
+                </Form.Group>
+            </Form>
         </div>
     )
 }
